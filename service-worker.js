d4e57e15927f9ec1818f1dd4109c3faeb9ddbf84
BrunoDemaze <brunodemaze482@gmail.com>
2026-04-04 16:33:26 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,5 @@
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open("prontoaqui").then(cache => {
-      return cache.addAll([
-        "/ProntoAqui/",
-        "/ProntoAqui/index.html",
-        "/ProntoAqui/servicos.html",
-        "/ProntoAqui/profissionais.html",
-        "/ProntoAqui/css/style.css",
-        "/ProntoAqui/js/app.js"
-      ])
-    })
-  )
+self.addEventListener("install", () => {
+  console.log("Service Worker instalado")
 })
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(response => {
-      return response || fetch(e.request)
-    })
-  )
-})
+self.addEventListener("fetch", () => {})
